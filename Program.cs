9eb39f71030492b18f1cf34fcc47fbea35b1247b
Лев <@CHUBA>
2021-06-16 11:13:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HomeTask_ForGIT_Chubar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Hello GIT");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
